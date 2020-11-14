32c7dee20fc74cc55c64d56f820b3d4c774fd4e5
Kengli Fu <kenglif12@gmail.com>
2020-11-14 10:58:04 -0800

    InOrder Done

--- src/main/java/Problem1/InOrderTraverse.java
+++ src/main/java/Problem1/InOrderTraverse.java
@@ -2,11 +2,32 @@ package Problem1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InOrderTraverse {
     public static List<Integer> inorderTraversalIterative(TreeNode<Integer> root) {
         // homework
         List<Integer> result = new ArrayList<>();
+        Stack<TreeNode<Integer>> stack = new Stack<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        TreeNode<Integer> current = root;
+
+        while (current != null || !stack.empty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+
+
+
         return result;  // place holder
     }
 }
